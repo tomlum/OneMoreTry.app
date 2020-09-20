6a27f9a01450f197ed064193a764b64182623518
tomlum <tomlumperson@gmail.com>
2020-09-19 22:58:34 -0400

    update faq

--- src/components/faq.js
+++ src/components/faq.js
@@ -3,6 +3,39 @@ import { useEffect, useRef, useState } from "preact/hooks";
 
 import styled from "styled-components";
 
+const AccordionHeader = styled.div`
+  background-color: #eceef4;
+  border: solid 3px #9b9ca1;
+  border-radius: 3px;
+  padding: 10px;
+  margin-top: 5px;
+  cursor: pointer;
+`;
+const AccordionBody = styled.div`
+  background-color: #eceef4;
+  border: solid 3px #9b9ca1;
+  border-top: solid 2px #9b9ca1;
+  border-radius: 3px;
+  padding: 10px;
+  margin-top: -3px;
+`;
+function Accordion({ title, content }) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      onClick={() => {
+        setOpen(!open);
+      }}
+    >
+      <AccordionHeader>
+        <b>{title}</b>
+      </AccordionHeader>
+      {open && <AccordionBody>{content}</AccordionBody>}
+    </div>
+  );
+}
+
 export const FAQButtonRing = styled.div`
   display: flex;
   justify-content: center;
@@ -34,13 +67,15 @@ export const Overlay = styled.div`
   border: solid 5px ${ThemeColor};
   border-radius: 10px;
   color: black;
-  background-color: #d5d7dc;
+  background-color: #e6e9ef;
   min-width: 700px;
   padding: 20px;
+  box-shadow: 0px 0px 10px 1px #333;
 
   h1 {
     font-size: 25px;
     margin: 0px;
+    margin-bottom: 4px;
   }
   h2 {
     font-size: 17px;
@@ -53,6 +88,10 @@ export const Overlay = styled.div`
     font-size: 17px;
     width: 100%;
     color: black;
+  }
+
+  a {
+    color: #4459c6;
   }
 
   span {
@@ -72,10 +111,6 @@ export const Overlay = styled.div`
   button {
     width: 30px;
     height: 30px;
-  }
-
-  .qa {
-    padding-top: 20px;
   }
 `;
 
@@ -104,6 +139,7 @@ export function FAQ({ setShowFAQ }) {
         </Row>
         <div className="flex flex1 justify-end">
           <button
+            className="flex justify-center align-center"
             onClick={() => {
               setShowFAQ(false);
             }}
@@ -116,157 +152,162 @@ export function FAQ({ setShowFAQ }) {
         Have a question? Reach out at{" "}
         <a href="https://twitter.com/tomlumperson">@TomLumPerson</a> on twitter
       </h2>
+      <Accordion
+        title={`⏱ What's the difference between "Delay" seconds and "Replay" seconds?`}
+        content={
+          <div>
+            Delay time is how long the camera is delayed for. Replay time is how
+            many seconds back in time before what you see on the screen to
+            record for the final replay. In total your final replay will be
+            roughly Delay + Replay seconds long.
+          </div>
+        }
+      />
+      <Accordion
+        title={"🎛 What do all the icon buttons mean?"}
+        content={
+          <div>
+            <div>
+              <span>
+                <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-mirror-icon.png"></img>
+                lets you mirror the camera
+              </span>
+            </div>
+            <div>
+              <span>
+                <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-align-icon.png"></img>
+                turns off the delay so you can setup the position of your camera
+              </span>
+            </div>
+            <div>
+              <span>
+                <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-mute-off.png"></img>
+                toggles muting the replay
+              </span>
+            </div>
+            <div>
+              <span>
+                <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-download.png"></img>
+                lets you download your replay
+              </span>
+            </div>
+          </div>
+        }
+      />
+      <Accordion
+        title={`🔊 Why can't I hear my audio playing back? / Why am I getting audio
+        feedback?`}
+        content={
+          <div>
+            While using the camera, there should be a little mute/unmute button
+            on the bottom right corner of the video. If you unmute, be sure to
+            be using headphones or a dedicated microphone, or else you may get
+            unwanted audio feedback.
+          </div>
+        }
+      />
+      <Accordion
+        title={`↔️ Why can't I control my replay?`}
+        content={
+          <div>
+            Depending on how long your replay video is, you may need to play it
+            once start to finish before the controls load. This is because the
+            video needs to load into the player first.
+          </div>
+        }
+      />
+      <Accordion
+        title={`🎥 Why is my download a .webm file?`}
+        content={
+          <div>
+            Unfortunately, due to technical limitations of browsers, that's the
+            default video file type. In order to keep OneMoreTry.app free and
+            fast, you'll have to do the file conversion yourself, my apologies!
+            I highly recommend using{" "}
+            <a href="https://handbrake.fr/">Handbrake</a>!
+          </div>
+        }
+      />
 
-      <div className="qa">
-        <div>
-          <b>
-            What's the difference between "Delay" seconds and "Replay" seconds?
-          </b>
-        </div>
-        <div>
-          Delay time is how long the camera is delayed for. Replay time is how
-          many seconds back in time before what you see on the screen to record
-          for the final replay. In total your final replay will be roughly Delay
-          + Replay seconds long.
-        </div>
-      </div>
+      <Accordion
+        title={`🏔 How would I use the app offline?`}
+        content={
+          <div>
+            <div>
+              If you're on chrome you can also install it as a dedicated app
+              <img
+                className="mt5"
+                src="https://s3.us-east-2.amazonaws.com/tomlum/omt-pwa-chrome.png"
+              ></img>
+            </div>
+            <br />
+            <div>
+              Otherwise, while you still have an internet connection, you can
+              visit the page. Once it's loaded, you can now use the app freely
+              without an internet connection. Also, most browsers will cache the
+              page, so if you visit it directly without an internet connection,
+              it may still work!
+            </div>
+          </div>
+        }
+      />
 
-      <div className="qa">
-        <div>
-          <b>What do all the icon buttons mean?</b>
-        </div>
-        <div>
-          <span>
-            <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-mirror-icon.png"></img>
-            lets you mirror the camera
-          </span>
-        </div>
-        <div>
-          <span>
-            <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-align-icon.png"></img>
-            turns off the delay so you can setup the position of your camera
-          </span>
-        </div>
-        <div>
-          <span>
-            <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-mute-off.png"></img>
-            toggles muting the replay
-          </span>
-        </div>
-        <div>
-          <span>
-            <img src="https://s3.us-east-2.amazonaws.com/tomlum/omt-download.png"></img>
-            lets you download your replay
-          </span>
-        </div>
-      </div>
+      <Accordion
+        title={`📱 Why doesn't this work on mobile/safari?`}
+        content={
+          <div>
+            Sadly, mobile browsers and safari are missing some features that are
+            essential for the delay camera to work (though I'm investigating
+            other solutions to get around this)
+          </div>
+        }
+      />
 
-      <div className="qa">
-        <div>
-          <b>
-            Why can't I hear my audio playing back? / Why am I getting audio
-            feedback?
-          </b>
-        </div>
-        <div>
-          While using the camera, there should be a little mute/unmute button on
-          the bottom right corner of the video. If you unmute, be sure to be
-          using headphones or a dedicated microphone, or else you may get
-          unwanted audio feedback.
-        </div>
-      </div>
+      <Accordion
+        title={`🔐 Do you collect my data? (No)`}
+        content={
+          <div>
+            Nope! The app works entirely locally on your computer. In fact,
+            after you visit the page, you don't even need an internet connection
+            to get it to work! If you're curious, this app is open source so you
+            can see the code for yourself{" "}
+            <a href="https://github.com/tomlum/OneMoreTry">here</a>!
+          </div>
+        }
+      />
 
-      <div className="qa">
-        <div>
-          <b>Why can't I control my replay?</b>
-        </div>
-        <div>
-          Depending on how long your replay video is, you may need to play it
-          once start to finish before the controls load. This is because the
-          video needs to load into the player first.
-        </div>
-      </div>
-
-      <div className="qa">
-        <div>
-          <b>Why is my download a .webm file?</b>
-        </div>
-        <div>
-          Unfortunately, due to technical limitations of browsers, that's the
-          default video file type. In order to keep OneMoreTry.app free and
-          fast, you'll have to do the file conversion yourself, my apologies! I
-          highly recommend using <a href="https://handbrake.fr/">Handbrake</a>!
-        </div>
-      </div>
-
-      <div className="qa">
-        <div>
-          <b>How would I use the app offline?</b>
-        </div>
-        <div>
-          First, while you still have an internet connection, visit the page.
-          Once it's loaded, you can now use the app freely without an internet
-          connection. Also, most browsers will cache the page, so if you visit
-          it directly without an internet connection, it may still work!
-        </div>
-      </div>
-
-      <div className="qa">
-        <div>
-          <b>Why doesn't this work on mobile/safari?</b>
-        </div>
-        <div>
-          Sadly, mobile browsers and safari are missing some features that are
-          essential for the delay camera to work (though I'm investigating other
-          solutions to get around this)
-        </div>
-      </div>
-
-      <div className="qa">
-        <div>
-          <b>Do you collect my data?</b>
-        </div>
-        <div>
-          Nope! The app works entirely locally on your computer. In fact, after
-          you visit the page, you don't even need an internet connection to get
-          it to work! If you're curious, this app is open source so you can see
-          the code for yourself{" "}
-          <a href="https://github.com/tomlum/OneMoreTry">here</a>!
-        </div>
-      </div>
-
-      <div className="qa">
-        <div>
-          <b>Can I donate to support this?</b>
-        </div>
-        <div>
-          I swear a real person actually asked me this. I really had no
-          intention of it but if you feel so inclined you can donate to support
-          this site and others here:
-          <form
-            action="https://www.paypal.com/cgi-bin/webscr"
-            method="post"
-            target="_top"
-          >
-            <input type="hidden" name="cmd" value="_donations" />
-            <input type="hidden" name="business" value="ZMHX8U6A5JARG" />
-            <input
-              type="hidden"
-              name="item_name"
-              value="To continue building and maintaining useful and free software tools!"
-            />
-            <input type="hidden" name="currency_code" value="USD" />
-            <input
-              type="image"
-              src="https://s3.us-east-2.amazonaws.com/tomlum/omt-donate-button.png"
-              border="0"
-              name="submit"
-              title="PayPal - The safer, easier way to pay online!"
-              alt="Donate with PayPal button"
-            />
-          </form>
-        </div>
-      </div>
+      <Accordion
+        title={`🤘 Can I donate to support this?`}
+        content={
+          <div>
+            I swear a real person actually asked me this. I really had no
+            intention of it but if you feel so inclined you can donate to
+            support this site and others here:
+            <form
+              action="https://www.paypal.com/cgi-bin/webscr"
+              method="post"
+              target="_top"
+            >
+              <input type="hidden" name="cmd" value="_donations" />
+              <input type="hidden" name="business" value="ZMHX8U6A5JARG" />
+              <input
+                type="hidden"
+                name="item_name"
+                value="To continue building and maintaining useful and free software tools!"
+              />
+              <input type="hidden" name="currency_code" value="USD" />
+              <input
+                type="image"
+                src="https://s3.us-east-2.amazonaws.com/tomlum/omt-donate-button.png"
+                border="0"
+                name="submit"
+                title="PayPal - The safer, easier way to pay online!"
+                alt="Donate with PayPal button"
+              />
+            </form>
+          </div>
+        }
+      />
     </Overlay>
   );
 }
